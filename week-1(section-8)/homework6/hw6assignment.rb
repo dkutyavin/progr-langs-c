884097d874326dcf8a17076b68768958b3d1077d
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-08-04 14:50:33 +0300

    hw6: "u" keybinding

--- week-1(section-8)/homework6/hw6assignment.rb
+++ week-1(section-8)/homework6/hw6assignment.rb
@@ -12,11 +12,57 @@ end
 
 class MyBoard < Board
   # your enhancements here
-
+  def rotate_180
+    if !game_over? and @game.is_running?
+      @current_block.move(0, 0, 2)
+    end
+    draw
+  end
 end
 
 class MyTetris < Tetris
   # your enhancements here
+  # creates the window and starts the game
+  def initialize
+    @root = TetrisRoot.new
+    @timer = TetrisTimer.new
+    set_board
+    @running = true
+    key_bindings
+    buttons
+    run_game
+  end
 
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
+
+  def key_bindings  
+    @root.bind('n', proc {self.new_game}) 
+
+    @root.bind('p', proc {self.pause}) 
+
+    @root.bind('q', proc {exitProgram})
+    
+    @root.bind('a', proc {@board.move_left})
+    @root.bind('Left', proc {@board.move_left}) 
+    
+    @root.bind('d', proc {@board.move_right})
+    @root.bind('Right', proc {@board.move_right}) 
+
+    @root.bind('s', proc {@board.rotate_clockwise})
+    @root.bind('Down', proc {@board.rotate_clockwise})
+
+    @root.bind('w', proc {@board.rotate_counter_clockwise})
+    @root.bind('Up', proc {@board.rotate_counter_clockwise}) 
+
+    @root.bind("u", proc {@board.rotate_180})
+    
+    @root.bind('space' , proc {@board.drop_all_the_way}) 
+  end
 end
 
