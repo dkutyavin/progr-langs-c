e637cc10124ea997eca7e23f974c00aca2d29191
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-09-08 19:16:39 +0300

    section 8 extra practice

--- week-2(section-9)/practice/section-8-solution.rb
+++ week-2(section-9)/practice/section-8-solution.rb
@@ -18,6 +18,7 @@ class Character
   private
 
   def play_out_encounter enc
+    raise "You should implement `play_out_encounter` for you Character subclass"
     ## YOUR CODE HERE
   end
 end
@@ -33,6 +34,28 @@ class Knight < Character
   end
 
   ## YOUR CODE HERE
+  def play_out_encounter enc
+    enc.encounter_knight self
+  end
+
+  def damage dam
+    if @ap == 0
+      @hp = @hp - dam
+    elsif @ap > dam
+      @ap = @ap - dam
+    else 
+      @hp = @hp - dam
+      @ap = 0
+    end
+  end
+
+  def drink_potion potion
+    @hp = @hp + potion.hp
+  end
+
+  def armor_up armor
+    @ap = @ap + armor.ap
+  end
 end
 
 class Wizard < Character
@@ -46,6 +69,40 @@ class Wizard < Character
   end
 
   ## YOUR CODE HERE
+  def play_out_encounter enc
+    enc.encounter_wizard self
+  end
+
+  def use_spell spell_mp
+    @mp = @mp - spell_mp
+  end
+
+  def damage dam
+    @hp = @hp - dam
+  end
+
+  def try_fly_over_trap trap
+    if @mp > 0
+      self.use_spell 1
+    else
+      self.damage trap.dam
+    end
+  end
+
+  def fight_monster monster
+    if @mp >= monster.hp
+      mana_needed_to_kill_a_monster = monster.hp
+      self.use_spell mana_needed_to_kill_a_monster
+    else
+      # wizard couldn't kill a monster, so monster killed him
+      @hp = 0
+    end
+  end
+
+  def drink_potion potion
+    @hp = @hp + potion.hp
+    @mp = @mp + potion.mp
+  end
 end
 
 class FloorTrap < Encounter
@@ -60,6 +117,13 @@ class FloorTrap < Encounter
   end
 
   ## YOUR CODE HERE
+  def encounter_knight knight
+    knight.damage @dam
+  end
+
+  def encounter_wizard wizard
+    wizard.try_fly_over_trap self
+  end
 end
 
 class Monster < Encounter
@@ -77,6 +141,13 @@ class Monster < Encounter
   end
 
   ## YOUR CODE HERE
+  def encounter_knight knight
+    knight.damage @dam
+  end
+
+  def encounter_wizard wizard
+    wizard.fight_monster self
+  end
 end
 
 class Potion < Encounter
@@ -93,6 +164,13 @@ class Potion < Encounter
   end
 
   ## YOUR CODE HERE
+  def encounter_knight knight
+    knight.drink_potion self
+  end
+
+  def encounter_wizard wizard
+    wizard.drink_potion self
+  end
 end
 
 class Armor < Encounter
@@ -108,10 +186,24 @@ class Armor < Encounter
   end
 
   ## YOUR CODE HERE
+  def encounter_knight knight
+    knight.armor_up self
+  end
+
+  def encounter_wizard wizard
+    # nothing to do here, just processing wizard to the next step
+    wizard
+  end
 end
 
+sir_foldalot = Knight.new(15, 3)
+knight_of_lambda_calculus = Knight.new(10, 10)
+sir_pinin_for_the_fjords = Knight.new(0, 15)
+alonzo_the_wise = Wizard.new(3, 50)
+dhuwe_the_unready = Wizard.new(8, 5)
+
 if __FILE__ == $0
-  Adventure.new(Stdout.new, Knight.new(15, 3),
+  Adventure.new(Stdout.new, alonzo_the_wise,
     [Monster.new(1, 1),
     FloorTrap.new(3),
     Monster.new(5, 3),
